cd546592e4010a91dd840ea132de264e1ecb4762
Yu <ygao0018@gmail.com>
2020-05-25 22:06:37 +1000

    new change

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = ({ resource }) => {
+const useResources = (resource) => {
 	const [resources, setResources] = useState([]);
 
 	const fetchResource = async (resource) => {
@@ -11,11 +11,23 @@ const ResourceList = ({ resource }) => {
 
 		setResources(response.data);
 	};
+
 	useEffect(() => {
 		fetchResource(resource);
-	}, []);
+	}, [resource]);
 
-	return <div>{resources.length}</div>;
+	return resources;
+};
+
+const ResourceList = ({ resource }) => {
+	const resources = useResources(resource);
+	return (
+		<ul>
+			{resources.map((record) => (
+				<li key={record.id}>{record.title}</li>
+			))}
+		</ul>
+	);
 };
 
 export default ResourceList;
